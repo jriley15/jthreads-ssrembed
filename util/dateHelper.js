9880f79c50006f0c2d7b6164e8c77bc8d19add4c
jriley15 <jordanr3@live.com>
2020-05-25 13:46:06 -0700

    improve getDateString

--- util/dateHelper.js
+++ util/dateHelper.js
@@ -1,17 +1,25 @@
 export const getDateString = (date) => {
-  let convertedDate = new Date(date + "Z");
+  let createdOn = new Date(date + "Z");
   let now = new Date();
-  if (
-    convertedDate.getFullYear() === now.getFullYear() &&
-    convertedDate.getMonth() === now.getMonth() &&
-    convertedDate.getDate() === now.getDate()
-  ) {
-    let time = convertedDate.toLocaleTimeString([], {
-      hour: "2-digit",
-      minute: "2-digit",
-    });
-    if (time.startsWith("0")) time = time.substring(1, time.length);
-    return "Today at " + time;
+  let difference = now.getTime() - createdOn.getTime();
+  let minutes = parseInt(difference / (1000 * 60), 10);
+  let hours = parseInt(difference / (1000 * 60 * 60), 10);
+  let days = parseInt(difference / (1000 * 60 * 60 * 24), 10);
+  let months = parseInt(difference / (1000 * 60 * 60 * 24 * 30), 10);
+  let years = parseInt(difference / (1000 * 60 * 60 * 24 * 30 * 12), 10);
+
+  if (minutes < 1) {
+    return "Just now";
+  } else if (hours < 1) {
+    return `${minutes} minute${minutes > 1 ? "s" : ""} ago`;
+  } else if (days < 1) {
+    return `${hours} hour${hours > 1 ? "s" : ""} ago`;
+  } else if (months < 1) {
+    return `${days} day${days > 1 ? "s" : ""} ago`;
+  } else if (years < 1) {
+    return `${months} month${months > 1 ? "s" : ""} ago`;
+  } else if (years > 0) {
+    return `${years} years${years > 1 ? "s" : ""} ago`;
   }
-  return convertedDate.toLocaleDateString();
+  return "";
 };
