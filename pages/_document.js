df090331a93ec25d0cdfd007885b6b0003225502
jriley15 <jordanr3@live.com>
2020-05-30 14:14:25 -0700

    remove og meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -10,11 +10,6 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
           />
-          <meta property="og:url"           content="https://www.your-domain.com/your-page.html" />
-          <meta property="og:type"          content="website" />
-          <meta property="og:title"         content="Your Website Title" />
-          <meta property="og:description"   content="Your description" />
-          <meta property="og:image"         content="https://www.your-domain.com/path/image.jpg" />
         </Head>
         <body>
           <Main />
