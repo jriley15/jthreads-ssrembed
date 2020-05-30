ee67d785be034356deb474f76e678661db2c5e5c
jriley15 <jordanr3@live.com>
2020-05-30 15:04:15 -0700

    build fix

--- components/comments/CreateComment.js
+++ components/comments/CreateComment.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { useSelector } from "react-redux";
 import { selectAuth } from "../../redux/authSlice";
 import useSWR from "swr";
